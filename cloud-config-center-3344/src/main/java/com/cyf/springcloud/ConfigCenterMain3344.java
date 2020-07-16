8e31a2fa224bea950cce5bd96e34c6866f2958af
kakk22 <1029839308@qq.com>
2020-07-16 21:53:27 +0800

    cloud-config

---
+++ cloud-config-center-3344/src/main/java/com/cyf/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.cyf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author by cyf
+ * @date 2020/7/16.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
---
+++ cloud-provider-payment8002/src/test/java/ZonedDateTimeDemo.java
@@ -0,0 +1,12 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author by cyf
+ * @date 2020/7/15.
+ */
+public class ZonedDateTimeDemo {
+    public static void main(String[] args) {
+        ZonedDateTime dateTime = ZonedDateTime.now();
+        System.out.println(dateTime);
+    }
+}
