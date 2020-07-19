1291fad8d481d867a370c64e1e6b16a525e39577
kakk22 <1029839308@qq.com>
2020-07-19 10:24:36 +0800

    springCloud alibaba nacos 初始化配置

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/cyf/springcloud/alibaba/controller/PaymentController.java
@@ -0,0 +1,21 @@
+package com.cyf.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author by cyf
+ * @date 2020/7/19.
+ */
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Long id){
+        return "serverPort:"+serverPort+"\t"+"id:"+id;
+    }
+}
