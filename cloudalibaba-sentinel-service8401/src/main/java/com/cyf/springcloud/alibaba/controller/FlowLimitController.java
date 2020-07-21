c10283917cd1012260fe2fd5cb89494e8e21b45c
kakk22 <1029839308@qq.com>
2020-07-21 21:09:40 +0800

    hothey sentinel

--- cloudalibaba-sentinel-service8401/src/main/java/com/cyf/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/cyf/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,6 +1,10 @@
 package com.cyf.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,15 +12,37 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/7/20.
  */
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/testA")
     public String testA(){
+        log.info(Thread.currentThread().getName());
         return "testA";
     }
 
     @GetMapping("/testB")
     public String testB(){
+
         return "testB";
     }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_hotKsy")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "testHotKey";
+    }
+
+    /**
+     * 兜底方法
+     * @param p1
+     * @param p2
+     * @param exception
+     * @return
+     */
+    public String deal_hotKsy (String p1, String p2, BlockException exception){
+        return "------deal_testHotKey,o(╥﹏╥)o";
+    }
+
 }
